9ed1fdd2760f4f9254eea5aa4d96429b017349ba
davies <sychopian@gmail.com>
2022-12-12 17:43:29 +0800

    Initial commit

---
+++ apps/admin/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'naraka-cloud-web-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.less'],
+})
+export class AppComponent {
+  title = 'admin';
+}
